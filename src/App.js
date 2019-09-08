4f041b623592c0c129824d9d0827745bede0908f
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-09-08 17:22:23 +0200

    Base style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+	return (
+		<div id="App">
+			<div>header</div>
+			<div id="wrapper">
+				<aside>
+					Aside
+				</aside>
+				<main>
+					main
+				</main>
+			</div>
+			<div>footer</div>
+		</div>
+	);
+};
 
 export default App;
