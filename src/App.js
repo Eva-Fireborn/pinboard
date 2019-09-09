a67f66441a700dcee20f6bdbcef498a79b8bf5d1
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-09-09 15:20:04 +0200

    starting to scss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Header from "./nav/Header.js";
 import Footer from "./nav/Footer.js";
 // Import of pages:
-// import PinboardView from "./pinboard/PinboardView.js"; 
+import PinboardView from "./pinboard/PinboardView.js";
 import AdsView from "./ads/AdsView.js";
-// import MsgView from "./msg/MsgView.js";
+import MsgView from "./msg/MsgView.js";
 
 const App = () => {
 	return (
