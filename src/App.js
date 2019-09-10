58fd05d2dede20f788dca9e67b2373de727cb845
Kristina Lomartire <kslomartire@hotmail.com>
2019-09-10 15:10:38 +0200

    Logo and profilepage html and scss

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Header from "./nav/Header.js";
 import Footer from "./nav/Footer.js";
 // Import of pages:
-// import PinboardView from "./pinboard/PinboardView.js"; 
+// import PinboardView from "./pinboard/PinboardView.js";
 // import AdsView from "./ads/AdsView.js";
 //import MsgView from "./msg/MsgView.js";
 import ContentRouting from './contentRouting';
+
 
 const App = () => {
 	return (
 		<div id="App">
 			<Router>
 				<Header />
-					<ContentRouting></ContentRouting>	
+				      <ContentRouting></ContentRouting>
 				<Footer />
 			</Router>
 		</div>
--- src/nav/Header.js
+++ src/nav/Header.js
@@ -5,26 +5,26 @@ const Header = () => {
 	const [links] = useState([
 		{name: "Annonser", to: "/annonser"},
 		{name: "Skapa annons", to: "/skapaannons"},
-        {name: "Frågor och svar", to: "/frågorochsvar"},
+    {name: "Frågor och svar", to: "/frågorochsvar"},
 		{name: "Logga in", to: "/loggain"},
 		{name: "Skapa konto", to: "/skapakonto"},
 	]);
-	
+
 	const navBar = links.map((link, index) => (
         <span key={index} >
             <Link to={link.to} > {link.name} </Link>
         </span>
 	));
-	
+
 	return (
 		<div id="header">
 			<a id="logo" href="index.html">
-				<img src={require('../img/tempLogo100.png')} alt="Pinboard" />
+				<img src={require('../img/pinboard.png')} alt="Pinboard" />
 			</a>
 
 			<nav>
 				{navBar}
-				
+
 				{/* <a href="ads.html">Annonser</a>
 				<a href="faq.html">Frågor och Svar</a>
 				<a href="profile.html">
