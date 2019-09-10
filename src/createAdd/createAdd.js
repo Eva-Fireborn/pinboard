938dae5d0c07964b84b3f0bcfa707051d58b15d9
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-09-10 15:38:59 +0200

    Update createAdd.js

--- src/createAdd/createAdd.js
+++ src/createAdd/createAdd.js
@@ -3,74 +3,75 @@ import React, { useState } from 'react';
 //const listWithData = [];
 
 const CreateAdd = () => {
-    const [value, setValue] = useState('');
-    //const [list, setList] = useState(listWithData);
+	const [value, setValue] = useState('');
+	//const [list, setList] = useState(listWithData);
 
-    const handleChange = event => {
-        setValue(event.target.value);
-    }
+	const handleChange = event => {
+		setValue(event.target.value);
+	}
 
-    const handleSubmit = event => {
-        // if (value) {
-        //     setList(list.concat({value}));
-        // }
-        // setValue('');
-        // console.log(list);
-        event.preventDefault();
-    }
+	const handleSubmit = event => {
+		// if (value) {
+		//     setList(list.concat({value}));
+		// }
+		// setValue('');
+		// console.log(list);
+		event.preventDefault();
+	}
 
-    
-    return (
-        <div id="wrapper">
-            <div className="formContainer">
-                <h5 className="rubrik">Skapa annons</h5>
-                <form onSubmit={handleSubmit}>
-                    <div className="checkboxes">
-                        <label>
-                            <input type="checkbox" /> Sökes
+
+	return (
+		<div id="wrapper">
+			<aside className="tipsContainer">
+				<h5 className="rubrik">Tips och råd</h5>
+				<div>
+					<p>Choose a realiable customer / provider</p>
+					<p>What's a good price for a service</p>
+					<p>How to write a good add</p>
+					<p>How to pay/get payed for the service</p>
+					<p>How to avoid fraud</p>
+				</div>
+			</aside>
+
+			<main className="formContainer">
+				<h5 className="rubrik">Skapa annons</h5>
+				<form onSubmit={handleSubmit}>
+					<div className="checkboxes">
+						<label>
+							<input type="checkbox" /> Sökes
                         </label>
-                        <label>
-                            <input type="checkbox" /> Finnes
+						<label>
+							<input type="checkbox" /> Finnes
                         </label>
-                    </div>
-                    <div className="addInfo">
-                        <label>Rubrik
+					</div>
+					<div className="addInfo">
+						<label>Rubrik
                             <input type="text" value={value} onChange={handleChange} />
-                        </label>
-                        <label>Kategori
+						</label>
+						<label>Kategori
                         <select name="categories">
-                            <option selected></option>
-				            <option value="musik">musik</option>
-				            <option value="mat">mat</option>
-                            <option value="transport">transport</option>
-                            <option value="hem">hem</option>
-                            <option value="städning">städning</option>
-			            </select>
-                        </label>
-                        <label>Beskrivning<textarea /></label>
-                        <label>Särskild önskemål<input /></label>
-                        <label>Stad<input /></label>
-                        <label>Gatuadress*<input /></label>
-                        <label className="shortInput" >Postnummer<input /></label>
-                        <label className="shortInput" >Pris<input  /></label>
-                       
-                    </div>
-                    <button type="submit" className="formButton">Publicera</button>
-                </form> 
-                <span>*valfri</span>  
-            </div>
-            <div className="tipsContainer">
-                <h5 className="rubrik">Tips och råd</h5>
-                <div>
-                    <p>Choose a realiable customer / provider</p>
-                    <p>What's a good price for a service</p>
-                    <p>How to write a good add</p>
-                    <p>How to pay/get payed for the service</p>
-                    <p>How to avoid fraud</p>
-                </div>
-            </div>
-        </div>
-    );
+								<option selected></option>
+								<option value="musik">musik</option>
+								<option value="mat">mat</option>
+								<option value="transport">transport</option>
+								<option value="hem">hem</option>
+								<option value="städning">städning</option>
+							</select>
+						</label>
+						<label>Beskrivning<textarea /></label>
+						<label>Särskild önskemål<input /></label>
+						<label>Stad<input /></label>
+						<label>Gatuadress*<input /></label>
+						<label className="shortInput" >Postnummer<input /></label>
+						<label className="shortInput" >Pris<input /></label>
+
+					</div>
+					<button type="submit" className="formButton">Publicera</button>
+				</form>
+				<span>*valfri</span>
+			</main>
+		</div>
+	);
 };
 
 export default CreateAdd;
