41844fd02057d5ec43d18ac473900cb783b84303
Ieva Lundin <eiwulia@gmail.com>
2019-09-17 17:20:45 +0200

    routing in footer and social media icons

--- src/contentRouting.js
+++ src/contentRouting.js
@@ -3,11 +3,15 @@ import { Switch, Route } from "react-router-dom";
 
 import AdsView from './ads/AdsView';
 import PinboardView from './pinboard/PinboardView';
-import CreateAdd from './createAdd/createAdd';
-import FrogorOchSvar from './fos/faq';
+import CreateAdd from './createAdd/CreateAdd';
+import FrogorOchSvar from './fos/Faq';
 import Login from './login/login';
 import ProfileView from './profile/ProfileView';
 import MsgView from './msg/MsgView';
+import Betalning from './fos/Betalning';
+import Disturbing from './fos/Disturbing';
+import Anvandarvillkor from './fos/Anvandarvillkor';
+import Kundsakerhet from './fos/Kundsakerhet';
 
 
 const ContentRouting = () => {
@@ -19,7 +23,11 @@ const ContentRouting = () => {
 		{ exact: false, path: "/frågorochsvar", component: FrogorOchSvar },
 		{ exact: false, path: "/loggain", component: Login },
 		{ exact: false, path: "/profil", component: ProfileView },
-		{ exact: false, path: "/meddelanden", component: MsgView }
+		{ exact: false, path: "/meddelanden", component: MsgView },
+		{ exact: false, path: "/betalning", component: Betalning },
+		{ exact: false, path: "/anmäl-annons", component: Disturbing },
+		{ exact: false, path: "/användarvillkor", component: Anvandarvillkor },	
+		{ exact: false, path: "/kundsäkerhet", component: Kundsakerhet },		
 	]);
 
 	const Routes = routes.map((route, index) => (
---
+++ src/fos/Betalning.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Betalning = () => {
+    return (
+        <h1>Betalning</h1>
+    )
+};
+
+export default Betalning;
