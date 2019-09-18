745d6b652b1d678fe3966d76fd5bbd2cc6162da1
Ieva Lundin <eiwulia@gmail.com>
2019-09-18 14:24:16 +0200

    merge conflict

--- src/nav/Header.js
+++ src/nav/Header.js
@@ -1,11 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-<<<<<<< HEAD
 import Login from './../login/login';
 import CreateUser from '../login/createUser';
-=======
->>>>>>> 64bfc9ff2a559acebcfd368d0e1cfa61ececceed
 import {
 	faChalkboardTeacher,
 	faPlusSquare,
@@ -15,7 +12,6 @@ import {
 	faEnvelope
 } from '@fortawesome/free-solid-svg-icons'
 
-<<<<<<< HEAD
 const Header = () => {
 	let [loginVisibility, changeLoginVisibility] = useState(false);
 	let [createUserVisibility, changeCreateUserVisibility] = useState(false);
@@ -39,21 +35,6 @@ const Header = () => {
 						<Link to="/annonser">
 							<FontAwesomeIcon icon={faChalkboardTeacher} />
 							Tjänster
-=======
-const Header = ({ isLoggedIn, activateLogin }) => {
-	return (
-		<div id="header">
-			<div id="fixedMenu">
-				<div id="logo">
-					<Link to="/">
-						<img src={require('../img/pinboard.png')} alt="Pinboard" />
-					</Link>
-				</div>
-				<nav>
-					<Link to="/annonser">
-						<FontAwesomeIcon icon={faChalkboardTeacher} />
-						Tjänster
->>>>>>> 64bfc9ff2a559acebcfd368d0e1cfa61ececceed
 						</Link>
 					<Link to="/skapaannons">
 						<FontAwesomeIcon icon={faPlusSquare} />
@@ -84,6 +65,7 @@ const Header = ({ isLoggedIn, activateLogin }) => {
 				</nav>
 			</div>
 		</div>
+	</div>
 	);
 };
 
