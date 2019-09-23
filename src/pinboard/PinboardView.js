c98aa46d6cc837dba3332bdb683c7c4ef46da3b7
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-09-23 10:54:17 +0200

    fixed style to work with pinboard start

--- src/pinboard/PinboardView.js
+++ src/pinboard/PinboardView.js
@@ -3,8 +3,8 @@ import React from "react";
 
 const PinboardView = () => {
 	return (
-		<div id="wrapper">
-			<main id="pinboard">
+		<div id="wrapper" className="pinboard">
+			<main className="pinboard">
 				All pinned messages...
 			</main>
 		</div>
