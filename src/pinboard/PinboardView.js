b9164a0f851659953a708aac7019313ae3109a89
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-09-23 14:53:55 +0200

    pinboard done, only need to change to latest 20 instead of all ads

--- src/pinboard/PinboardView.js
+++ src/pinboard/PinboardView.js
@@ -1,47 +1,52 @@
 import React from "react";
+import SingleAdCard from "../ads/SingleAdCard.js";
+
+class PinboardView extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			listOfAds: [],
+		}
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:4000/ApiGetAllAds')
+			.then(res => res.json())
+			.then((result) => {
+				let parsedResult = JSON.parse(result.body);
+				let ads = [];
+				parsedResult.forEach(res => {
+					ads.push(res)
+				});
+				this.setState({
+					listOfAds: ads
+				});
+			},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					console.log(error)
+				}
+			)
+	}
 
 
-const PinboardView = () => {
-	return (
-		<div id="wrapper" className="pinboard">
-			<main className="pinboard">
-				<ul>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-					<li>
-						<h2>Roliga grejer</h2>
-						<p>Men en lång beskrivning av vad det är för något.</p>
-					</li>
-				</ul>
-			</main>
-		</div>
-	);
+	render() {
+		return (
+			<div id="wrapper" className="pinboard">
+				<main className="pinboard">
+					<ul>
+						{this.state.listOfAds && this.state.listOfAds.length ?
+							this.state.listOfAds.map((ad, key) =>
+								<SingleAdCard key={key} adObject={ad} />)
+							:
+							null}
+					</ul>
+				</main>
+			</div>
+		)
+	}
 };
 
 export default PinboardView;
