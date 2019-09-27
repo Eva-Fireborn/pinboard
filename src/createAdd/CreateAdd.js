33647c60be2fd019914a543ad0b027e5e10a9e2d
Ieva Lundin <eiwulia@gmail.com>
2019-09-27 14:13:33 +0200

    user message when ad is send and clean form after submit. New colors and checkboxes

--- src/createAdd/CreateAdd.js
+++ src/createAdd/CreateAdd.js
@@ -3,6 +3,7 @@ import { Formik, Field, FieldArray, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import ReviewAd from './ReviewAd';
 import ErrorMsg from './ErrorMsg';
+import GreenMessage from './GreenMessage';
 
 
 const CreateAdd = ({ isLoggedIn }) => {
@@ -10,17 +11,19 @@ const CreateAdd = ({ isLoggedIn }) => {
 	const [valid, setIsValid] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
 	const [userID, setUserID] = useState(null);
+	const [messageSent, setMessageSent] = useState(false);
 
     useEffect(() => {
-        if (isLoggedIn)
-            setUserID(isLoggedIn._id);
-        else
-            setUserID(null)
+        if (isLoggedIn) {
+			setUserID(isLoggedIn._id);
+		} else {
+			setUserID(null)
+		}
     }, [isLoggedIn]);
 
-	let categories = [{ id: 1, name: 'Söker' }, { id: 2, name: ' Säljer' }];
+	let categories = [{ id: 1, name: 'Söker' }, { id: 2, name: 'Säljer' }];
 
-	async function sendNewAd(fields, resetForm) {
+	async function sendNewAd(fields) {
         let ad = {...fields, userId: userID};
 		const serverResponse = await fetch('http://localhost:4000/ApiPostNewAd', {
 			method: 'POST',
@@ -32,10 +35,8 @@ const CreateAdd = ({ isLoggedIn }) => {
 		const res = await serverResponse.json();
 		console.log('Ad created successfuly! Response status: ', res.status);
 		if (res.status === 200) {
-			console.log('yay');
-			Formik.resetForm();
-			//resetForm({}); //add object so it works
-			//value={values.short || ''} // <= Suggested change
+			setMessageSent(true);
+			setTimeout(() => setMessageSent(false), 2000);
 		}
 	}
 
@@ -43,13 +44,13 @@ const CreateAdd = ({ isLoggedIn }) => {
 		setIsValid(false);
 		if (!isLoggedIn && !isValid) {
 			setIsValid(true);
-			setErrorMessage('You have to log in first and fill in the required fields')
+			setErrorMessage('Du måste logga in för att publicera en annons och alla obligatoriska fälten måste vara fyllt i')
 		} else if (isValid && !isLoggedIn) {
 			setIsValid(true);
-			setErrorMessage('You have to log in first');
+			setErrorMessage('Du måste logga in för att publicera en annons');
 		} else if (isLoggedIn && !isValid) {
 			setIsValid(true);
-			setErrorMessage('You have to fill in the required fields')
+			setErrorMessage('Du måste fylla i alla obligatoriska fälten')
 		} else if (isValid && isLoggedIn) {
 			setErrorMessage('');
 		}
@@ -59,7 +60,7 @@ const CreateAdd = ({ isLoggedIn }) => {
         <div id="wrapper">
             <main className="formContainer">
                 <h3 className="rubrik">Skapa annons</h3>
-                <Formik 
+				<Formik 
                     initialValues={{
                         addType: '',
                         header: '',
@@ -91,14 +92,15 @@ const CreateAdd = ({ isLoggedIn }) => {
                             //.min(-1, 'Pris kan inte vara negativ')
                             .required('Skriv ett pris')
                     })}
-                    onSubmit={(fields, resetForm) => {
+                    onSubmit={(fields, {resetForm}) => {
                         if (isLoggedIn) {
-                            sendNewAd(fields, resetForm);
+							sendNewAd(fields);
+							resetForm();
                         } else {
                             console.log('trying to submit without login in');
                         }
                     }}
-                    render={({ errors, touched, values, isValid }) => (
+                    render={({ errors, touched, values, isValid, isSubmitting}) => (
                         <Form>
                             <div>
                             <label htmlFor="type">Annons typ*</label>
@@ -136,14 +138,14 @@ const CreateAdd = ({ isLoggedIn }) => {
                             </div>
                             </div>
 
-							<div className="form-group">
+							<div>
 								<label htmlFor="header">Rubrik*</label>
-								<Field name="header" type="text" className={'form-control' + (errors.header && touched.header ? ' is-invalid' : '')} />
+								<Field name="header" type="text" className={errors.header && touched.header ? ' is-invalid' : ''} placeholder="ex. Musik lärare" />
 								<ErrorMessage name="header" component="div" className="invalid-feedback" />
 							</div>
-							<div className="form-group">
+							<div>
 								<label htmlFor="category">Kategori*</label>
-								<Field component="select" name="category" type="select" className={'form-control select' + (errors.category && touched.category ? ' is-invalid' : '')}>
+								<Field component="select" name="category" type="select" className={'select' + (errors.category && touched.category ? ' is-invalid' : '')} >
 									<option value=""></option>
 									<option value="djur">Djur</option>
 									<option value="fritid">Fritid/hobby</option>
@@ -156,39 +158,39 @@ const CreateAdd = ({ isLoggedIn }) => {
 								</Field>
 								<ErrorMessage name="category" component="div" className="invalid-feedback" />
 							</div>
-							<div className="form-group">
+							<div>
 								<label htmlFor="description">Beskrivning*</label>
-								<Field component="textarea" name="description" type="textarea" className={'form-control' + (errors.description && touched.description ? ' is-invalid' : '')} />
+								<Field component="textarea" name="description" type="textarea" className={errors.description && touched.description ? ' is-invalid' : ''} placeholder="tips: annonstext borde vara beskrivande av tjänsten och längre än 20 tecken"/>
 								<ErrorMessage name="description" component="div" className="invalid-feedback" />
 							</div>
-							<div className="form-group">
+							<div>
 								<label htmlFor="specialRequirements">Särskild önskemål</label>
-								<Field name="specialRequirements" type="text" />
+								<Field name="specialRequirements" type="text" placeholder="ex. kan läsa noter" />
 							</div>
-							<div className="form-group">
+							<div>
 								<label htmlFor="city">Stad*</label>
-								<Field name="city" type="text" className={'form-control' + (errors.city && touched.city ? ' is-invalid' : '')} />
+								<Field name="city" type="text" className={errors.city && touched.city ? ' is-invalid' : ''} placeholder="ex. Göteborg" />
 								<ErrorMessage name="city" component="div" className="invalid-feedback" />
 							</div>
-							<div className="form-group">
+							<div>
 								<label htmlFor="street">Gatuadress</label>
-								<Field name="street" type="text" />
+								<Field name="street" type="text" placeholder="ex. Storgatan 1A" />
 							</div>
 							<div className="shortInputs">
-								<div className="form-group shortInput">
+								<div className="shortInput">
 									<label htmlFor="zip">Postnummer*</label>
-									<Field name="zip" type="text" className={'form-control' + (errors.zip && touched.zip ? ' is-invalid' : '')} />
+									<Field name="zip" type="text" className={errors.zip && touched.zip ? ' is-invalid' : ''} placeholder="ex. 40530" />
 									<ErrorMessage name="zip" component="div" className="invalid-feedback" />
 								</div>
-								<div className="form-group shortInput">
+								<div className="shortInput">
 									<label htmlFor="price">Pris* (kr)</label>
-									<Field name="price" type="text" className={'form-control' + (errors.price && touched.price ? ' is-invalid' : '')} />
+									<Field name="price" type="text" className={errors.price && touched.price ? ' is-invalid' : ''} placeholder="ex. 100" />
 									<ErrorMessage name="price" component="div" className="invalid-feedback" />
 								</div>
 							</div>
 							<div className="obligatorisk">*obligatorisk</div>
-							<div className="form-group">
-								<button type="submit" className="formButton call" onClick={() => UserInformation(isValid)}>Publicera</button>
+							<div>
+								<button type="submit" className="formButton call" onClick={() => UserInformation(isValid)} disabled={isSubmitting}>Publicera</button>
 								{/* <button type="reset" className="resetButton" onClick={() => setIsValid(false)}>Återställ</button> */}
 								<button type="button" onClick={() => setVisibility(!visibility)}>Förhandsgranska annons</button>
 							</div>
@@ -198,6 +200,7 @@ const CreateAdd = ({ isLoggedIn }) => {
 					)}
 				/>
 				{valid ? <ErrorMsg>{errorMessage}</ErrorMsg> : null}
+				{messageSent ? <GreenMessage /> : null}
 			</main>
 		</div>
 	);
