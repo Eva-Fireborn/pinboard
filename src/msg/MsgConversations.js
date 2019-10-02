3559c00bb991d81b2759976a63755a57e3460002
Viktor <43089746+vlorenzen@users.noreply.github.com>
2019-10-02 11:39:02 +0200

    small msg style update

--- src/msg/MsgConversations.js
+++ src/msg/MsgConversations.js
@@ -4,6 +4,7 @@ const MsgConversations = ({ msg, showConversations }) => {
 	return (
 		<div className="msgRoom" onClick={() => showConversations(msg)}>
 			<h3>{msg.adHeader}</h3>
+			<h5>{msg.senderName}</h5>
 		</div>
 	);
 };
